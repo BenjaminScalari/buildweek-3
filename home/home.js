961ba9534e5887c0e35dfdde7484103535460596
Mik3007 <michelealtieri3007@gmail.com>
2024-05-14 21:06:22 +0200

    completata la funzione per la card principale della home

--- home/home.js
+++ home/home.js
@@ -1,34 +1,34 @@
-const url = "https://striveschool-api.herokuapp.com/api/deezer/search?q=geolier"
+const url = "https://striveschool-api.herokuapp.com/api/deezer/search?q=geolier/"
 
-window.onload = async() => {
-    try{
+async function loadSongs() {
     const response = await fetch(url);
     const songs = await response.json();
     console.log(songs);
 
-    let divArtist = document.getElementById("card-hero-home")
-
-    divArtist.innerHTML = dischi.map(song => `
+    const divArtist = document.getElementById("card-hero-home");
+    if (divArtist && songs.data.length > 0) {
+        const firstSong = songs.data[0]; // Ottieni solo la prima canzone
+        divArtist.innerHTML = `
             <div class="d-flex">
                 <div>
-                    <img src="https://picsum.photos/100/100" alt="">
+                    <img id="cover-image" src="${firstSong.album.cover_xl}" alt="Foto">
                 </div>
                 <div class="d-flex flex-column mx-3">
                     <span>ALBUM</span>
-                    <h3>${song.title}</h3>
-                    <span class="fs-6">${song.name}</span>
-                    <span>Ascolta il tuo nuovo singolo di ${song.name}</span>
+                    <h3 id"song-title">${firstSong.title}</h3>
+                    <span class="fs-6">${firstSong.artist.name}</span>
+                    <span>Ascolta il tuo nuovo singolo di ${firstSong.artist.name}</span>
                     <div> 
                         <button class="button-play-home" type="button"> Play </button>
                         <button class="button-salva-home" type="button"> Salva </button>
                     </div>
                 </div>
-                <div>
-                    <span class="ms-5">4 ore</span>
-                </div>
             </div>
-        `).join('');
-    }  catch (error){
-        console.error('error', error )
-    }    
+        `;
+    } else {
+        // Gestione se non ci sono canzoni disponibili
+        divArtist.innerHTML = "Nessuna canzone trovata";
+    }
 }
+
+loadSongs();
